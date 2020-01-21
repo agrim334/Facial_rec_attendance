8adfa971b921cd6666ae4b852c5db8c64ec6e64b
agrim334 <42838221+agrim334@users.noreply.github.com>
2020-01-21 15:49:38 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
-export FLASK_APP=microblog.py
+export FLASK_APP=yourappname
 export FLASK_ENV=development
 export MAIL_SERVER=smtp.googlemail.com
 export MAIL_PORT=587
 export MAIL_USE_TLS=1
-export MAIL_USERNAME=agrim334@gmail.com
-export MAIL_PASSWORD=dumbledoreisdead
+export MAIL_USERNAME=yourmail
+export MAIL_PASSWORD=yourpassword
 
