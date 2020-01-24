73ebe7409600023b7eba3513e545e05e9ea7ac09
Agrim <agrim334@gmail.com>
2020-01-24 11:40:48 +0530

    reseting

--- env.sh
+++
@@ -1,8 +0,0 @@
-export FLASK_APP=yourappname
-export FLASK_ENV=development
-export MAIL_SERVER=smtp.googlemail.com
-export MAIL_PORT=587
-export MAIL_USE_TLS=1
-export MAIL_USERNAME=yourmail
-export MAIL_PASSWORD=yourpassword
-
