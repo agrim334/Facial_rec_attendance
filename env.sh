7b29736e1946f7445a59e2e7ee29540f3a0a4365
Agrim <agrim334@gmail.com>
2020-03-11 12:14:13 +0530

    env removed

--- env.sh
+++
@@ -1,7 +0,0 @@
-export FLASK_APP=microblog.py
-export FLASK_ENV=development
-export MAIL_SERVER=smtp.googlemail.com
-export MAIL_PORT=587
-export MAIL_USE_TLS=1
-export MAIL_USERNAME=Youremail
-export MAIL_PASSWORD=yourpassword
